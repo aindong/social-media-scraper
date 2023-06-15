25637a261f1f9223981a6321be42056eea0dba72
Alleo Indong <alleo.indong@gmail.com>
2023-06-15 18:55:19 +0800

    feat: initial scraper in action

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,25 @@
+import chalk from 'chalk';
 import puppeteer from 'puppeteer-extra';
 import StealthPlugin from 'puppeteer-extra-plugin-stealth';
 
 async function main() {
+  console.info(chalk.blue('Launching social media scraper by aindong'));
+
   puppeteer.use(StealthPlugin());
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch({
+    headless: 'new',
+    defaultViewport: {
+      width: 1920,
+      height: 1080,
+    },
+  });
+
   const page = await browser.newPage();
 
-  await page.goto('https://bot.sannysoft.com/');
-  await page.waitForSelector('img', { visible: true });
-  await page.screenshot({ path: 'bot-test-stealth.png' });
+  await page.goto('https://www.tiktok.com/@mrnigelng');
+  // wait for 5 secs
+  await new Promise((r) => setTimeout(r, 5000));
+  await page.screenshot({ path: 'screen-capture.png' });
 
   await browser.close();
 }

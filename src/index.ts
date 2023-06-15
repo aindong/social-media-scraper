9ff1caf983535200fbf005b75a58ec5383c394f1
Alleo Indong <alleo.indong@gmail.com>
2023-06-16 01:52:06 +0800

    feat: main function and crawler factory

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,65 @@
 import chalk from 'chalk';
 import puppeteer from 'puppeteer-extra';
 import StealthPlugin from 'puppeteer-extra-plugin-stealth';
+import { waitForTimeout } from './utils/waitForTimeout';
+import { Crawler } from './types/crawler';
+import { InstagramCrawler } from './social-media/instagram.crawler';
+import { TiktokCrawler } from './social-media/tiktok.crawler';
+import { Config } from './utils/config';
 
 async function main() {
   console.info(chalk.blue('Launching social media scraper by aindong'));
 
   puppeteer.use(StealthPlugin());
   const browser = await puppeteer.launch({
-    headless: 'new',
+    headless: Config.get('CRAWLER_ENV') === 'production' ? 'new' : false,
     defaultViewport: {
       width: 1920,
       height: 1080,
     },
   });
 
-  const page = await browser.newPage();
+  const profiles = [
+    'https://www.tiktok.com/@mrnigelng',
+    // 'https://www.instagram.com/mrnigelng/',
+  ];
 
-  await page.goto('https://www.tiktok.com/@mrnigelng');
-  // wait for 5 secs
-  await new Promise((r) => setTimeout(r, 5000));
-  await page.screenshot({ path: 'screen-capture.png' });
+  console.info(chalk.blue(`Crawling ${profiles.length} profiles`));
+  while (profiles.length > 0) {
+    const profile = profiles.pop();
+    if (profile) {
+      let crawler: Crawler | null = null;
+      // check what social media it is
+      // if tiktok, create a tiktok crawler
+      // if instagram, create an instagram crawler
+      if (profile.includes('instagram.com')) {
+        // create a tiktok crawler
+        crawler = new InstagramCrawler(browser, profile);
+      }
+
+      if (profile.includes('tiktok.com')) {
+        // create a tiktok crawler
+        crawler = new TiktokCrawler(browser, profile);
+      }
+
+      if (!crawler) {
+        console.error(chalk.red(`No crawler found for ${profile}`));
+        continue;
+      }
+
+      const result = await crawler.crawl();
+      console.info(chalk.blue(`Done crawling ${profile}`));
+      console.log(result);
+    }
+  }
+  console.info(chalk.blue('Done crawling'));
+
+  // const page = await browser.newPage();
+
+  // await page.goto('https://www.tiktok.com/@mrnigelng');
+  // // wait for 5 secs
+  // await waitForTimeout(5000);
+  // await page.screenshot({ path: 'screen-capture.png' });
 
   await browser.close();
 }

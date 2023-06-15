29917893618a728c7696262e79b1cd23d632a55e
Alleo Indong <alleo.indong@gmail.com>
2023-06-15 18:03:07 +0800

    feat: stealth mode for puppeteer

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('Hello world!')
+import puppeteer from 'puppeteer-extra';
+import StealthPlugin from 'puppeteer-extra-plugin-stealth';
+
+async function main() {
+  puppeteer.use(StealthPlugin());
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+
+  await page.goto('https://bot.sannysoft.com/');
+  await page.waitForSelector('img', { visible: true });
+  await page.screenshot({ path: 'bot-test-stealth.png' });
+
+  await browser.close();
+}
+
+main().catch(console.error);
